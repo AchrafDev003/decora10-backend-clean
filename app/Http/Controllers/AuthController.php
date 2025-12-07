17f11571c3ed0fdd1366bb4ec8861920679e20c1
Achraf Fennane <hrafartist@gmail.com>
2025-12-07 22:47:08 +0100

    Fix login4

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,9 +36,7 @@ class AuthController extends Controller
             return response()->json(['error' => 'Credenciales incorrectas'], 401);
         }
 
-        if (!$user->hasVerifiedEmail()) {
-            return response()->json(['error' => 'Email no verificado'], 403);
-        }
+
 
 
         // ⚡ Crear token solo si email verificado
