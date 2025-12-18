63c0cfdc63a5a60b4e017344d01bb4e9cacf92e2
Achraf Fennane <hrafartist@gmail.com>
2025-12-18 10:45:35 +0100

    Update prod6

--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class UpdateProductRequest extends FormRequest
 {
@@ -17,28 +16,18 @@ class UpdateProductRequest extends FormRequest
         $id = $this->route('id');
 
         return [
-            'id_product' => [
-                'required',
-                'string',
-                Rule::unique('products', 'id_product')->ignore($id),
-            ],
-
             'name' => 'required|string|max:255',
+            //'slug' => "required|string|unique:products,slug,$id",
             'description' => 'nullable|string',
             'price' => 'required|numeric|min:0',
             'promo_price' => 'nullable|numeric|min:0',
             'is_promo' => 'nullable|boolean',
             'promo_ends_at' => 'nullable|date|after_or_equal:today',
             'quantity' => 'required|integer|min:1',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:2048',
+
             'category_id' => 'required|exists:categories,id',
-
-            // ✅ CLAVE
-            // UpdateProductRequest
-            'images' => 'nullable|array',
-            'images.*' => 'image|mimes:jpg,jpeg,png,webp|max:4096',
-            'delete_images' => 'nullable|array',
-            'delete_images.*' => 'integer|exists:product_images,id',
-
+            'id_product' => "required|string|unique:products,id_product,$id",
         ];
     }
 }
