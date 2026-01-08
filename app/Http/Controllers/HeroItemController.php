f944f6c7659c3dec047bb9c1dc40e8356d07e4a6
Achraf Fennane <hrafartist@gmail.com>
2026-01-08 10:25:23 +0100

    update hero

--- app/Http/Controllers/HeroItemController.php
+++ app/Http/Controllers/HeroItemController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\HeroItem;
+use Cloudinary\Api\Upload\UploadApi;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Storage;
