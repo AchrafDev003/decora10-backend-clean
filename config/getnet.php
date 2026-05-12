0abbd621c46230bef5a2495fcdc0be633e791b42
Achraf Fennane <hrafartist@gmail.com>
2026-05-12 11:42:20 +0200

    getnet14

---
+++ config/getnet.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'merchant' => env('GETNET_MERCHANT'),
+    'terminal' => env('GETNET_TERMINAL'),
+    'secret'   => env('GETNET_SECRET'),
+    'url'      => env('GETNET_URL'),
+];
